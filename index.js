8fd2bca9f7ad33ac4d40a602e8546dfe0b81af3d
Juan <jcabarique@uninorte.edu.co>
2020-01-09 22:30:24 -0500

    Se agrego el modulo express y se creo un servidor

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo")
+const express = require('express')
+const app = express()
+
+//Config
+app.set('port', 3000)
+
+//middleware
+
+
+//Routes
+
+
+app.listen(app.get('port'), () => {
+    console.log('Conectado al puerto ', app.get('port'))
+})
